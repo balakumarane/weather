257f5c823d9c2b9106f979b640aaddbe7de37df3
BK <balakumaran.e@gmail.com>
2018-06-15 08:49:19 +0530

    Modified docker settings
    Modified settings

--- start.sh
+++ start.sh
@@ -4,9 +4,16 @@
 echo Starting Gunicorn.
 python manage.py migrate
 echo ....
+# Prepare log files and start outputting logs to stdout
+mkdir -p /code/logs
+touch /code/logs/gunicorn.log
+touch /code/logs/gunicorn-access.log
+tail -n 0 -f /code/logs/gunicorn*.log &
+
+
 exec gunicorn weather.wsgi:application \
 	            --bind 0.0.0.0:8000 \
-                --workers 1
-# --log-file /tmp/gunicorn.log \
-# --log-level INFO 
-
+                --workers 1\
+                --log-level=info \
+                --log-file=/code/logs/gunicorn.log \
+                --access-logfile=/code/logs/gunicorn-access.log \
