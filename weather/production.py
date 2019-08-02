0666e6cacad6f8cf34871bd28c25607bbde3b9d1
balakumaran <bala.e@amtexsystems.com>
2019-08-02 14:51:21 +0530

    static url

--- weather/production.py
+++ weather/production.py
@@ -1,5 +1,5 @@
 from .settings import *
-from .pwa_settings import *
+
 # STATIC_URL = "https://rawgit.com/balakumarane/weather/v2.0/weather_app/static/" 
 
 # STATIC_URL = "https://cdn.jsdelivr.net/gh/balakumarane/weather@vv2.0/weather_app/static/"
@@ -18,4 +18,19 @@ DATABASES = {
 DEBUG=False
 
 
-# Django PWA
+# Minio Storage
+DEFAULT_FILE_STORAGE="minio_storage.storage.MinioMediaStorage"
+STATICFILES_STORAGE="minio_storage.storage.MinioStaticStorage"
+MINIO_STORAGE_ENDPOINT="bk.sytes.net"
+MINIO_STORAGE_ACCESS_KEY="balakumaran"
+MINIO_STORAGE_SECRET_KEY="yajiv@92"
+MINIO_STORAGE_USE_HTTPS=True
+MINIO_STORAGE_MEDIA_BUCKET_NAME="weather-media"
+MINIO_STORAGE_AUTO_CREATE_MEDIA_BUCKET=True
+MINIO_STORAGE_STATIC_BUCKET_NAME = 'weather-static'
+MINIO_STORAGE_AUTO_CREATE_STATIC_BUCKET = True
+
+# Get static Url from minio
+
+# Django Progressive Web App
+from .pwa_settings import *
